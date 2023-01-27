d57afb95d3927eb114533d47fa32f9dff5965ce0
Jaloliddin7 <abdutolipovjaloliddin913@gmail.com>
2023-01-27 22:21:30 +0500

    Tugadi

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-const burgerbtn = document.querySelector(".burger__btn");
-const menyubar = document.querySelector(".menyu__bar");
-const close = document.querySelector(".header__close");
+// const burgerbtn = document.querySelector(".burger__btn");
+// const menyubar = document.querySelector(".menyu__bar");
+// const close = document.querySelector(".header__close");
 
-burgerbtn.addEventListener("click",()=> {
-    menyubar.classList.toggle("menyu__togle")
-    console.log("Salom");
+// burgerbtn.addEventListener("click",()=> {
+//     menyubar.classList.toggle("menyu__togle")
+//     console.log("Salom");
 
-})
-close.addEventListener("click",()=> {
-    menyubar.classList.toggle("menyu__togle")
-})
+// })
+// close.addEventListener("click",()=> {
+//     menyubar.classList.toggle("menyu__togle")
+// })
+function katta   (x){
+
+    // let a = [1,2,10,5,5,12];
+    let b = "";
+    for (var i = 0; i < x; i++){
+             b+="*";
+        }
+    console.log(b);   
+
+}
+katta(50    );
+
+
+
+
